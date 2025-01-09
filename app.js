df1a6b73d32ffee3c927469658b197a7b055c94b
jatinsi16 <jatinsingh1655@gmail.com>
2025-01-09 22:03:44 +0530

    added api key to js

---
+++ app.js
@@ -0,0 +1,3 @@
+const API_KEY ="922e54470aba716cce3c3570e4d1ff0e";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast";
